038b9d0bb8363e8cc7df710a731d0478af742ea2
SOUMYADIP GIRI <122915461+Soumyadipgithub@users.noreply.github.com>
2025-04-03 01:50:04 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-def evaluate(df):  
-    """Force computation for FireDucks' lazy evaluation"""  
-    try:  
-        # FireDucks-specific evaluation  
-        df._evaluate()  
-    except AttributeError:  
-        # No action needed for Pandas/other tools  
-        pass  
